4f6a1f9189f215c7ba3e08242d3b9236226c4909
Aeryan <raunoita@gmail.com>
2021-01-10 01:39:08 +0200

    Letter placement and independent movement

--- scrabble.py
+++ scrabble.py
@@ -1,11 +1,16 @@
 import os
+import math
 import arcade
 from letters import *
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 900
+BOARD_WIDTH = 900
+BOARD_HEIGHT = 900
+SLOT_WIDTH = 60
+SLOT_HEIGHT = 60
 # SCREEN_HEIGHT = 720
-SCREEN_TITLE = "Basic board"
+SCREEN_TITLE = "Snakes and Scrabbles"
 # Dictionary for storing all letters and their respective point values
 # (https://www.thewordfinder.com/scrabble-point-values.php)
 LETTERS_DICTIONARY = {'A': 1, 'B': 3, 'C': 3}
@@ -26,10 +31,20 @@ class ScrabbleGame(arcade.Window):
 
         self.background = None
 
-        self.tile = arcade.SpriteList()
+        # Selectable and movable letter blocks
+        self.active_blocks = arcade.SpriteList()
+        # Homeward bound and temporarily unselectable letter blocks
+        self.moving_blocks = arcade.SpriteList()
+        # Temporarily placed and unselectable blocks
+        self.pending_blocks = arcade.SpriteList()
+        # Unselectable and unmovable letter blocks
+        self.inactive_blocks = arcade.SpriteList()
 
         self.heldLetter = None
-        self.heldLetter_InitialPos = None
+
+        # Transposed board matrix with rows as tuples: [*zip(*self.board)]
+        # Transposed board matrix with rows as lists: list(map(list, [*zip(*self.board)]))
+        self.board = [[None for j in range(15)] for i in range(15)]
 
     # Call to restart game
     def setup(self):
@@ -40,37 +55,78 @@ class ScrabbleGame(arcade.Window):
             new_letter.center_x = 1100
             new_letter.center_y = new_letter.center_y + vertical_offset
             vertical_offset = vertical_offset + 150
-            self.tile.append(new_letter)
+            self.active_blocks.append(new_letter)
         self.heldLetter = None
-        self.heldLetter_InitialPos = ()
 
     # Called to calculate new frame
     def on_update(self, delta_time):
-        pass
+        self.active_blocks.on_update(delta_time)
+        self.inactive_blocks.on_update(delta_time)
+        self.moving_blocks.on_update(delta_time)
+
+        for block in self.moving_blocks:
+            if not block.movement:
+                self.moving_blocks.remove(block)
+                if block.is_home():
+                    self.active_blocks.append(block)
+                else:
+                    self.pending_blocks.append(block)
 
     # Called on frame draw
     def on_draw(self):
         arcade.start_render()
 
         arcade.draw_lrwh_rectangle_textured(0, 0,
-                                            900, 900,
+                                            BOARD_WIDTH, BOARD_HEIGHT,
                                             self.background)
-        for letters in self.tile:
-            letters.draw()
+        self.active_blocks.draw()
+        self.inactive_blocks.draw()
+        self.pending_blocks.draw()
+        self.moving_blocks.draw()
+
+    # Checks board when the RETURN (ENTER) key is pressed
+    def on_key_press(self, symbol: int, modifiers: int):
+        if symbol == arcade.key.RETURN:
+            self.check_board()
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """ Called when the user presses a mouse button. """
-        self.heldLetter = arcade.get_sprites_at_point((x, y), self.tile)[0]
+        self.heldLetter = (arcade.get_sprites_at_point((x, y), self.active_blocks) or [None])[0]
 
     def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
         """ Called when the user presses a mouse button. """
-        self.heldLetter = None
+        if self.heldLetter is not None:
+            self.active_blocks.remove(self.heldLetter)
+            self.moving_blocks.append(self.heldLetter)
+            if len(arcade.get_sprites_at_point((x, y), self.inactive_blocks)) == 0 and x < BOARD_WIDTH:
+                self.heldLetter.place(self.nearest_cell(x, y))
+            else:
+                self.heldLetter.return_home()
+            self.heldLetter = None
 
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
         """ User moves mouse """
         if self.heldLetter is not None:
             self.heldLetter.center_x += dx
             self.heldLetter.center_y += dy
+
+    def nearest_cell(self, x, y):
+        return [SLOT_WIDTH * math.floor(x/SLOT_WIDTH) + SLOT_WIDTH / 2,
+                SLOT_HEIGHT * math.floor(y/SLOT_HEIGHT) + SLOT_HEIGHT / 2]
+
+    def check_board(self):
+        # TODO: implement board state checking (iterate over slots on every row in both the board matrix and
+        #  the transposed board matrix and check if every uninterrupted sequence of characters is a word using
+        #  the checker from wordchecker.py).
+        #  If a non-word character sequence is found, retract the last move (return the letter blocks to their "homes").
+        #  If all character sequences are valid words, find all added or changed words by comparing the board state
+        #  with the state before the last move, calculate the added score, and update the score and
+        #  the scoreboard (once implemented)
+        # Placeholder action: moves letters to their starting positions
+        for block in self.pending_blocks:
+            block.return_home()
+            self.moving_blocks.append(block)
+        self.pending_blocks = arcade.SpriteList()
 
 
 if __name__ == "__main__":
