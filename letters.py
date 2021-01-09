801545bf8606f28711e65089bf5c7936b680ac84
Shreya Ponugoti <sp843@cornell.edu>
2021-01-09 23:04:41 +0700

    removed getter and hidden attributes

--- letters.py
+++ letters.py
@@ -3,15 +3,12 @@ LETTERS_DICTIONARY = {'A':1,'B':3,'C':3}
 
 class Letter(arcade.Sprite):
     """
-    Attribute letter: A string that denotes which alphabet each Letter object represents
+    Attribute letter_string: A string that denotes which alphabet each Letter object represents
+    Attribute points: Number of points associated with each letter, according to Scrabble rules
     """
-    #Hidden attributes
-    #Attribute _points: Number of points associated with each letter, according to Scrabble rules
-    def getLetter(self):
-        return self.letter
 
     def __init__(self, alphabet):
         filename = "images/"+alphabet+".png"
         super().__init__(filename)
-        self._points = LETTERS_DICTIONARY[alphabet]
-        self.letter = alphabet
+        self.points = LETTERS_DICTIONARY[alphabet]
+        self.letter_string = alphabet
