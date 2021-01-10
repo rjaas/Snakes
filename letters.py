03a40179f5f4ffa6e59b81241d4bb6a60807f5e6
Shreya Ponugoti <sp843@cornell.edu>
2021-01-10 19:15:50 +0700

    Updated letters dict

--- letters.py
+++ letters.py
@@ -1,6 +1,6 @@
 import os
 import arcade
-LETTERS_DICTIONARY = {'A': 1, 'B': 3, 'C': 3}
+LETTERS_DICTIONARY = {'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2}
 
 
 class Letter(arcade.Sprite):
